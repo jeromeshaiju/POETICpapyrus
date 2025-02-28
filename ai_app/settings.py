1f52778b3059627e5ab87258c32ecdd23359f9d3
jeromeshaiju <164854743+jeromeshaiju@users.noreply.github.com>
2025-02-28 12:25:50 +0530

    Update settings.py

--- ai_app/settings.py
+++ ai_app/settings.py
@@ -21,11 +21,6 @@ ALLOWED_HOSTS = ["POETICpapyrus.onrender.com"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
 
