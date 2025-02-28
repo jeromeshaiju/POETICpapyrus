d91889a349798f2037cec41c15088ff01cd28f44
jeromeshaiju <164854743+jeromeshaiju@users.noreply.github.com>
2025-02-28 11:16:28 +0530

    Update settings.py

--- ai_app/settings.py
+++ ai_app/settings.py
@@ -66,12 +66,6 @@ WSGI_APPLICATION = 'ai_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 import dj_database_url
 import os
