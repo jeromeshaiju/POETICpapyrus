8d4c32469479d24d57767290626536d0ddb22da4
jeromeshaiju <164854743+jeromeshaiju@users.noreply.github.com>
2025-02-28 12:35:19 +0530

    Update settings.py

--- ai_app/settings.py
+++ ai_app/settings.py
@@ -61,9 +61,11 @@ WSGI_APPLICATION = 'ai_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.dummy'  # This prevents Django from expecting a real DB
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
